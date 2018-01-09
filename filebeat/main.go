7b74773583c8c3bd93d1eaf5f0d5e6c815ed005b
freelingchang <freelingchang@gmail.com>
2018-01-09 15:23:56 +0800

    add 1

--- filebeat/main.go
+++ filebeat/main.go
@@ -15,6 +15,7 @@ import (
 // Finally, prospector uses the registrar information, on restart, to
 // determine where in each file to restart a harvester.
 func main() {
+	//此去执行 github.com/elastic/beats/libbeat/cmd.genRunCmd.func1
 	if err := cmd.RootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
